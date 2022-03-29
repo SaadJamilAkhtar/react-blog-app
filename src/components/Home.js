3f697e02edf139b580bb0c3647805dd3bdf01685
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-29 11:11:49 +0500

    conditional rendering done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,28 @@ import BlogList from "./BlogList";
 
 function Home(props) {
 
-    const [blogs, setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState(null);
+
+    useEffect(() => {
+        getData()
+    },[])
+
+    const getData = async () => {
+        const data = await fetch('http://localhost:5000/blogs');
+        setBlogs(await data.json());
+    }
 
     const delBlog = (id) => {
         setBlogs(blogs.filter(blog => blog.id !== id));
     }
 
-    useEffect(() => {
-        const getData = async () => {
-            const data = await fetch('http://localhost:5000/blogs');
-            setBlogs(await data.json());
-        }
-        getData()
-    },[])
+
     return (
         <div className="home">
             <div className={"page-heading"}>
                 <h1>Recent Blogs</h1>
             </div>
-            {blogs.map(blog => {
+            { blogs && blogs.map(blog => {
                 return (
                     <BlogList key={blog.id} blog={blog} onDelete={delBlog}/>
                 )
