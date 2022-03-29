0329a8b151cf344040c329be9e33c9974e44da85
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-29 23:40:04 +0500

    error message done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import {FaTimesCircle} from "react-icons/fa";
 import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
+
 function Home(props) {
 
-    const [blogs, setBlogs] = useState(null);
+    const [blogs, setBlogs] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         getData();
-    },[])
+    }, [])
 
     const getData = async () => {
         setIsLoading(true);
-        const data = await fetch('http://localhost:5000/blogs');
-        setBlogs(await data.json());
+        try {
+            const data = await fetch('http://localhost:5000/blogss');
+            if (!data.ok) {
+                throw new Error("Could not Fetch data.")
+            } else {
+                setError(null);
+                setBlogs(await data.json());
+            }
+        } catch (e) {
+            setIsLoading(false);
+            setError(e.message);
+        }
         setIsLoading(false);
     }
 
@@ -28,8 +41,23 @@ function Home(props) {
             <div className={"page-heading"}>
                 <h1>Recent Blogs</h1>
             </div>
+
+            {/*Error Block*/}
+            {error &&
+            <div className="loading error">
+                <FaTimesCircle style={
+                    {color:"#ee2f2f",fontSize: '2rem'}
+                }/>
+                <h2>
+                    {error}
+                </h2>
+            </div>}
+
+            {/*Loading Block*/}
             {isLoading && <div className={"loading"}><h2>Loading Data...</h2></div>}
-            { blogs && blogs.map(blog => {
+
+            {/*Blog block*/}
+            {blogs && blogs.map(blog => {
                 return (
                     <BlogList key={blog.id} blog={blog} onDelete={delBlog}/>
                 )
