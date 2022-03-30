d4d34cbb44c386d3c4d90721505f7db104d126cf
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-31 04:45:26 +0500

    form handling

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function BlogForm(props) {
+    const [data, setData] = useState({
+        title: "",
+        body: "",
+        author: "mario"
+    });
+
+    const changeHandler = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
         <div className={'create'}>
             <h2>New Blog</h2>
-            <form>
+            <form onSubmit={submit}>
                 <label>Blog Title</label>
                 <input
                     type="text"
                     required
+                    value={data.title}
+                    name="title"
+                    onChange={changeHandler}
                 />
 
                 <label>Content</label>
                 <textarea
                     required
-                ></textarea>
+                    name="body"
+                    value={data.body}
+                    onChange={changeHandler}
+                />
                 <label>Blog Author</label>
-                <select>
+                <select name="author" value={data.author} onChange={changeHandler}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
 
-                <button>Add Blog</button>
+                <button type={"submit"}>Add Blog</button>
             </form>
         </div>
     );
