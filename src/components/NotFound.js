5f07544aa84e071173c54d47ae21a0cfaf50a3df
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-04 12:16:45 +0500

    minor style tweaks

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {FaArrowCircleLeft} from "react-icons/fa";
+import {FaArrowCircleLeft, FaTimesCircle} from "react-icons/fa";
 
 function NotFound(props) {
     return (
         <div className={"not-found"}>
-            <h2>HTTP 404</h2>
+            <div className={'main-error'}>
+                    <FaTimesCircle style={
+                        {fontSize: '4rem', paddingTop: '0.75rem', paddingRight: '2rem',
+                        marginLeft:"15%"}
+                    }/>
+                <h2>HTTP 404</h2>
+            </div>
             <p>Page not found!</p>
-            <Link to={'/'}><span className="icon"><FaArrowCircleLeft/></span>Back to homepage...</Link>
+            <Link to={'/'}>
+                <span className="icon">
+                    <FaArrowCircleLeft style={
+                        {color: "#f1356d", fontSize: '2rem'}
+                    }/>
+                </span>
+                Back to homepage...
+            </Link>
         </div>
     );
 }
